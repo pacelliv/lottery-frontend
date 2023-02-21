7ef16c91e88521fe608dd26d9430c241bb7d762f
PacelliV <flores.eugenio03@gmail.com>
2023-02-20 20:23:39 -0500

    new icons

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,14 @@
-import Link from "next/link"
+import { FaTwitter, FaGithub } from "react-icons/fa"
 import footerStyles from "../src/styles/Footer.module.css"
 
 const Footer = () => {
     return (
         <div className={footerStyles.footer}>
             <a href="https://github.com/pacelliv" target="_blank">
-                <img src="../images/github-logo.png" />
+                <FaGithub className={footerStyles.faIcon} />
             </a>
             <a href="https://twitter.com/pacelliv3" target="_blank">
-                <img src="../images/twitter-logo.png" />
+                <FaTwitter className={footerStyles.faIcon} />
             </a>
         </div>
     )
